cc53030ff8cf159eb7cda88067b21ef6fad1873c
Lea Foy <leafoy@gmail.com>
2022-08-30 15:41:23 -0400

    Rearranging routes to be unavailable if not logged in, added userContext but not using -- may do so later.

--- client/src/App.js
+++ client/src/App.js
@@ -5,51 +5,70 @@ import Homepage from "./components/pages/Homepage";
 import LoggedOutWelcome from "./components/pages/LoggedOutWelcome";
 import { useAuth0 } from "@auth0/auth0-react";
 import SearchPage from "./components/pages/SearchPage";
-import { BookSearchContextProvider } from "./components/CurrentBookSearch";
+import { BookSearchContext } from "./components/CurrentBookSearch";
 import BookPage from "./components/pages/BookPage";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "./components/UserContext";
 
 const App = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
+
+    // const { siteUser, setSiteUser } = useContext(UserContext);
+
+    // const determineLoginState = async () => {
+    //     await isAuthenticated;
+    //     isAuthenticated && setSiteUser({email: user.email, });
+    // };
+
+    // determineLoginState();
+
 
     return (
         <BrowserRouter>
             <GlobalStyles />
             {/* Only show header if user signed in */}
-            <Header/>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        // Redirect to Welcome page if not signed in
-                        !isLoading &&
-                        (isAuthenticated ? (
-                            <Homepage />
-                        ) : (
-                            <Navigate replace to="/welcome" />
-                        ))
-                    }
-                />
-                <Route path="/search/isbn/:isbn" />
-                <Route path="/search/authors/:author_id" />
-                <Route path="/search/:search_terms" />
-                <Route
-                    path="/search"
-                    element={
-                        // !isLoading &&
-                        // (isAuthenticated ? (
-                        //     <SearchPage />
-                        // ) : (
-                        //     <Navigate replace to="/welcome" />
-                        // ))
-                        <SearchPage />
-                    }
-                />
-                <Route path="/shelf" />
-                <Route path="/book" element={<BookPage />} />
-                <Route path="/friends" />
-                <Route path="/user" />
-                <Route path="/welcome" element={<LoggedOutWelcome />} />
-            </Routes>
+            {!isLoading &&
+                (isAuthenticated ? (
+                    <>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Homepage />} />
+                            <Route path="/search/isbn/:isbn" />
+                            <Route path="/search/authors/:author_id" />
+                            <Route path="/search/:search_terms" />
+                            <Route path="/search" element={<SearchPage />} />
+                            <Route path="/shelf" />
+                            <Route path="/book" element={<BookPage />} />
+                            <Route path="/friends" />
+                            <Route path="/user" />
+                            <Route
+                                path="/welcome"
+                                element={<LoggedOutWelcome />}
+                            />
+                        </Routes>
+                    </>
+                ) : (
+                    <>
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={
+                                    // Redirect to Welcome page if not signed in
+                                    !isLoading &&
+                                    (isAuthenticated ? (
+                                        <Homepage />
+                                    ) : (
+                                        <Navigate replace to="/welcome" />
+                                    ))
+                                }
+                            />
+                            <Route
+                                path="/welcome"
+                                element={<LoggedOutWelcome />}
+                            />
+                        </Routes>
+                    </>
+                ))}
         </BrowserRouter>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import { BookSearchContextProvider } from "./components/CurrentBookSearch";
+import { UserContextProvider } from "./components/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        <BookSearchContextProvider>
-            <Auth0Provider
-                domain="dev-cq-a6k-9.us.auth0.com"
-                clientId="1rP8o2guroF4kP2wdmcZ9YW4zAPkJBCS"
-                redirectUri="http://localhost:3000"
-            >
-                <App />
-            </Auth0Provider>
-        </BookSearchContextProvider>
+        <UserContextProvider>
+            <BookSearchContextProvider>
+                <Auth0Provider
+                    domain="dev-cq-a6k-9.us.auth0.com"
+                    clientId="1rP8o2guroF4kP2wdmcZ9YW4zAPkJBCS"
+                    redirectUri="http://localhost:3000"
+                >
+                    <App />
+                </Auth0Provider>
+            </BookSearchContextProvider>
+        </UserContextProvider>
     </>
-    // document.getElementById("root")
 );
