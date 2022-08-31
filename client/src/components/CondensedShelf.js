d94d949c913af941588bfc9551ae6e951c48cf97
Lea Foy <leafoy@gmail.com>
2022-08-30 20:59:47 -0400

    Add addShelf modal. Created separate component but not currently using it

--- client/src/components/CondensedShelf.js
+++ client/src/components/CondensedShelf.js
@@ -1,47 +1,104 @@
 import styled from "styled-components";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { UserContext } from "./UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Modal from "react-modal";
+Modal.setAppElement(document.getElementById("root"));
 
 // This component goes on the homepage and displays the user's shelves
 const CondensedShelf = () => {
     const { shelves, setShelves } = useContext(UserContext);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    let subtitle;
 
     const addShelf = () => {
-        return (
-            
-        )
+        setModalOpen(true);
+    };
+
+    // const afterOpenModal = () => {
+    //     subtitle.style.color = '#f00';
+    // }
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleNewShelfSubmit = (e) => {
+        e.preventDefault();
+        
     }
+
     return (
         <StyledShelf>
             <h2 class="shelves">My Shelves</h2>
             <Tabs className="tabs-outer">
-            {shelves.length > 0 ? (
-                <>
-                <TabList className="tab-list">
-                    <Tab className="tab">Title 1</Tab>
-                    <Tab className="tab">Title 2</Tab>
-                </TabList>
+                {shelves.length > 0 ? (
+                    <>
+                        <TabList className="tab-list">
+                            <Tab className="tab">Title 1</Tab>
+                            <Tab className="tab">Title 2</Tab>
+                        </TabList>
 
-                <TabPanel className="tab-panel">
-                    <h2>content</h2>
-                </TabPanel>
-                <TabPanel className="tab-panel">
-                    <h2>content2</h2>
-                </TabPanel>
-                </>
-            ):(
-                <>
-                <TabList className="tab-list">
-                    <Tab className="tab" onClick={addShelf}>+ Add shelf</Tab>
-                </TabList>
+                        <TabPanel className="tab-panel">
+                            <h2>content</h2>
+                        </TabPanel>
+                        <TabPanel className="tab-panel">
+                            <h2>content2</h2>
+                        </TabPanel>
+                    </>
+                ) : (
+                    <>
+                        <TabList className="tab-list">
+                            <Tab className="tab" onClick={addShelf}>
+                                + Add shelf
+                            </Tab>
+                        </TabList>
 
-                <TabPanel className="tab-panel">
-                    <h2>content</h2>
-                </TabPanel>
-                </>
-            )}
+                        <TabPanel className="tab-panel">
+                            <h2>content</h2>
+                        </TabPanel>
+                    </>
+                )}
             </Tabs>
+            <Modal
+                isOpen={modalOpen}
+                // onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                contentLabel="Add shelf"
+            >
+                <div className="add-modal">
+                    <button className="close-btn" onClick={closeModal}>
+                        x
+                    </button>
+                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+                        Add a new bookshelf
+                    </h2>
+                    <div>
+                        Add a shelf name (for example, "To Read" or "Favourite
+                        Mysteries") and a description.
+                    </div>
+                    <form onSubmit={handleNewShelfSubmit}>
+                        <label for="shelf-name">
+                            Shelf name:
+                            <input
+                                type="text"
+                                id="shelf-name"
+                                name="shelf-name"
+                            />
+                        </label>
+                        <label for="description">
+                            Description:
+                            <input
+                                type="text"
+                                id="description"
+                                name="description"
+                            />
+                        </label>
+                        <button type="submit">Add shelf</button>
+                    </form>
+                </div>
+            </Modal>
         </StyledShelf>
     );
 };
@@ -85,6 +142,16 @@ const StyledShelf = styled.div`
             &:focus {
                 display: block;
             }
+        }
+    }
+
+    .add-modal {
+        .close-btn{
+            background-color: blue;
+        } 
+        form {
+            display: flex;
+            flex-direction: column;
         }
     }
 `;
