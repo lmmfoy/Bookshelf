eeef33c2c9d9a94e52c0a51e1d24a812aa775fec
Lea Foy <leafoy@gmail.com>
2022-09-01 14:34:32 -0400

    Add addBook function

--- server/handlers/updateUser.js
+++ server/handlers/updateUser.js
@@ -12,23 +12,58 @@ const options = {
 
 const client = new MongoClient(MONGO_URI, options);
 
-const updateShelf = async (req, res) => {
+const addBook = async (db, email, shelf, res) => {
+    const user = await db.collection("users").findOne({ email: email });
+
+    let added;
+
+    // Map through the user's shelves to find the correct one to add the book to
+    // if no books are added yet, set up the shelf with its first book
+    // otherwise, filter the books to make sure the new book isn't already on the shelf
+    // If the length of the filtered list is 0, then push the new book to the shelf
+    const updatedShelf = user.shelves.map((item) => {
+        item.name === shelf.name &&
+            (item.books
+                ? item.books.filter((book) => {
+                      return book.key === shelf.books.key;
+                  }).length === 0 && item.books.push(shelf.books)
+                : (item.books = [shelf.books]));
+        return item;
+    });
+
+    added = await db
+        .collection("users")
+        .updateOne({ email: email }, { $set: { shelves: updatedShelf } });
+
+    res.status(200).json({ status: 200, data: added });
+};
+
+const addShelf = async (req, res) => {
     const { email, shelf } = req.body;
-    console.log(email, shelf);
+
     try {
         await client.connect();
         const db = client.db("Users");
 
-        const added = await db
-            .collection("users")
-            .update({ email: email }, { $push: {shelves: shelf}});
-        console.log(added);
-        res.status(200).json({ status: 200, data: added });
+        if (shelf.books) {
+            addBook(db, email, shelf, res);
+            // const added = await db
+            //     .collection("users")
+            //     .updateOne({ email: email }, { $set: { shelves: test2 } });
+            // console.log(added);
+            // res.status(200).json({ status: 200, data: added });
+        } else {
+            const added = await db
+                .collection("users")
+                .updateOne({ email: email }, { $push: { shelves: shelf } });
+            console.log(added);
+            res.status(200).json({ status: 200, data: added });
+        }
     } catch (err) {
         res.status(400).json({ status: 400, data: err.message });
     }
 };
 
 module.exports = {
-    updateShelf,
+    addShelf,
 };
