3a5c2b5c62d7d38296146829297ec90ec8db691f
Lea Foy <leafoy@gmail.com>
2022-09-02 14:25:15 -0400

    Update to render pages based on the user state rather than Auth0

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import SearchPage from "./components/pages/SearchPage";
 import BookPage from "./components/pages/BookPage";
 import { useEffect, useState, useContext } from "react";
-// import { UserContext } from "./components/UserContext";
+import { UserContext } from "./components/UserContext";
 
 const App = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
-
+    const { siteUser } = useContext(UserContext);
     // const { siteUser, setSiteUser } = useContext(UserContext);
 
     // const determineLoginState = async () => {
@@ -21,53 +21,54 @@ const App = () => {
 
     // determineLoginState();
 
-
     return (
         <BrowserRouter>
             <GlobalStyles />
-            {/* Only show header if user signed in */}
-            {!isLoading &&
-                (isAuthenticated ? (
-                    <>
-                        <Header />
-                        <Routes>
-                            <Route path="/" element={<Homepage />} />
-                            <Route path="/search/isbn/:isbn" />
-                            <Route path="/search/authors/:author_id" />
-                            <Route path="/search/:search_terms" />
-                            <Route path="/search" element={<SearchPage />} />
-                            <Route path="/shelf" />
-                            <Route path="/book" element={<BookPage />} />
-                            <Route path="/friends" />
-                            <Route path="/user" />
-                            <Route
-                                path="/welcome"
-                                element={<LoggedOutWelcome />}
-                            />
-                        </Routes>
-                    </>
-                ) : (
-                    <>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={
-                                    // Redirect to Welcome page if not signed in
-                                    !isLoading &&
-                                    (isAuthenticated ? (
-                                        <Homepage />
-                                    ) : (
-                                        <Navigate replace to="/welcome" />
-                                    ))
-                                }
-                            />
-                            <Route
-                                path="/welcome"
-                                element={<LoggedOutWelcome />}
-                            />
-                        </Routes>
-                    </>
-                ))}
+            {/* Only show if user signed in */}
+            {siteUser ? (
+                <>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/search/isbn/:isbn" />
+                        <Route path="/search/authors/:author_id" />
+                        <Route path="/search/:search_terms" />
+                        <Route path="/search" element={<SearchPage />} />
+                        <Route path="/shelf" />
+                        <Route path="/book" element={<BookPage />} />
+                        <Route path="/friends" />
+                        <Route path="/user" />
+                        <Route path="/welcome" element={<LoggedOutWelcome />} />
+                    </Routes>
+                </>
+            ) : (
+                <>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                // Redirect to Welcome page if not signed in 
+                                // Checks against Auth0 rather than siteUser as siteUser is set after the user is redirected to the homepage
+                                !isLoading &&
+                                (isAuthenticated ? (
+                                    <Homepage />
+                                ) : (
+                                    <Navigate replace to="/welcome" />
+                                ))
+                            }
+                        />
+                        <Route
+                            path="/search"
+                            element={<Navigate replace to="/welcome" />}
+                        />
+                        <Route
+                            path="/book"
+                            element={<Navigate replace to="/welcome" />}
+                        />
+                        <Route path="/welcome" element={<LoggedOutWelcome />} />
+                    </Routes>
+                </>
+            )}
         </BrowserRouter>
     );
 };
