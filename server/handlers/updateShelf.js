0e7fab2f51e77f21d8f51e2bdd2031f1696d56ca
Lea Foy <leafoy@gmail.com>
2022-09-03 12:08:41 -0400

    Update comments for updateShelf functions and update bookDetailsSpecific so only one shelf can be checked and the user is alerted to whether the book was added to shelf

--- server/handlers/updateShelf.js
+++ server/handlers/updateShelf.js
@@ -12,6 +12,7 @@ const options = {
 
 const client = new MongoClient(MONGO_URI, options);
 
+// This function adds a book to one of the user's bookshelves if it is not already added (it is called below in the updateShelf function)
 const addBook = async (db, email, shelf, res) => {
     const user = await db.collection("users").findOne({ email: email });
 
@@ -31,6 +32,9 @@ const addBook = async (db, email, shelf, res) => {
         return item;
     });
 
+    // Update the shelf to the updated shelf value. If no change was made to the shelf
+    // (i.e. if the book was already on the shelf and updatedShelf didn't actually change)
+    // the data sent to the frontend will include "modifiedCount: 0" (if modified, will include "modifiedCount: 1")
     added = await db
         .collection("users")
         .updateOne({ email: email }, { $set: { shelves: updatedShelf } });
@@ -38,14 +42,21 @@ const addBook = async (db, email, shelf, res) => {
     res.status(200).json({ status: 200, data: added });
 };
 
+// Add a new shelf to the database if it doesn't already exist
+// If it already exists, the response will be sent with "modifiedCount: 0"
+// This function is called in updateShelf
 const addShelf = async (db, email, shelf, res) => {
     const added = await db
         .collection("users")
-        .updateOne({ email: email, "shelves.name": {$ne: shelf.name} }, {$addToSet: {  shelves: shelf } });
+        .updateOne(
+            { email: email, "shelves.name": { $ne: shelf.name } },
+            { $addToSet: { shelves: shelf } }
+        );
 
     res.status(200).json({ status: 200, data: added });
 };
 
+// This function checks whether the user is trying to add a book to a shelf or add a new shelf, then calls the appropriate function
 const updateShelf = async (req, res) => {
     const { email, shelf } = req.body;
 
